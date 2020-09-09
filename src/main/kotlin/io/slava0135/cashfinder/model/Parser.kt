49271bbfea425649d5f7ccc186e963e655326dec
slava0135 <super.novalskiy_0135@inbox.ru>
2020-09-09 21:45:56 +0300

    parsing...

--- src/main/kotlin/io/slava0135/cashfinder/model/Parser.kt
+++ src/main/kotlin/io/slava0135/cashfinder/model/Parser.kt
@@ -4,20 +4,46 @@ fun parse(lines: List<String>): List<Node> {
     TODO()
 }
 
-private fun validate(lines: List<String>): Boolean {
-    require(lines.size > 2 && lines[0].length > 4)
-    val crosses = mutableSetOf<Int>()
+private val rowRegex = Regex("""([+](-+|\s+))+[+]""")
+private val colRegex = Regex("""([+]([|]|\s))+[+]""")
 
-    for (index in lines[0].indices) {
+fun validate(lines: List<String>): String {
+    //unified length
+    if (lines.any { it.length != lines.first().length }) return "Different line lengths"
 
-        if (lines[index] == "+") crosses.add(index)
-    }
-
-    for (i in 2..lines.size step 2) {
-        for (j in lines[i].indices) {
-            if (j in crosses) {
-
-            }
+    //checking rows
+    if (lines.first().any { it !in "+-" } || lines.last().any { it !in "+-" }) return "Bad upper/bottom border"
+    for (row in lines.indices) {
+        if (row % 2 == 0) {
+            if (!rowRegex.matches(lines[row]) || lines[row].replace(" ", "-") != lines.first()) return "Row #$row is wrong"
         }
     }
+
+    //reading crosses positions
+    val crosses = mutableSetOf<Int>()
+    for (index in lines[0].indices) {
+        if (lines[0][index] == '+') {
+            crosses.add(index)
+        }
+    }
+
+    //minimum requirements
+    if (crosses.size < 4 || lines.size < 3) return "Maze is too small"
+
+    //checking columns
+    fun rotate(index: Int) = lines.map { it[index] }.joinToString("")
+    val firstCol = rotate(0)
+    if (firstCol.any { it !in "+|" } || rotate(lines[0].length - 1).any { it !in "+|" }) return "Bad left/right border"
+    for (col in lines[0].indices) {
+        val line = rotate(col)
+        if (col in crosses) {
+            if (!colRegex.matches(line)) return "Column #$col is wrong"
+        }
+    }
+    return ""
+}
+
+fun main() {
+    val lines = {}.javaClass.getResource("/test1").readText().split("\n")
+    println(validate(lines))
 }
