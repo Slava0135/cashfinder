a2aeb21e652a45c7a4e97ee6cd0cbb07f10ba116
slava0135 <super.novalskiy_0135@inbox.ru>
2020-09-25 17:11:24 +0300

    moving everything in Graph.kt

--- src/main/kotlin/io/slava0135/cashfinder/model/Graph.kt
+++ src/main/kotlin/io/slava0135/cashfinder/model/Graph.kt
@@ -31,6 +31,113 @@ class Graph(val width: Int, val height: Int) {
     var start: Node? = null
     var end: Node? = null
 
+    companion object Factory {
+        private val rowRegex = Regex("""([+](-+|\s+))+[+]""") // +--+-+---+
+        private val colRegex = Regex("""([+]([|]|\s))+[+]""") // +||+|+|||+
+
+        fun read(lines: List<String>): Graph {
+            //checking lines lengths
+            if (lines.any { it.length != lines.first().length })
+                throw IllegalArgumentException("Different lines lengths")
+
+            //checking rows
+            if (lines.first().any { it !in "+-" } || lines.last().any { it !in "+-" })
+                throw IllegalArgumentException("Bad upper/bottom border")
+            for (row in lines.indices) {
+                if (row % 2 == 0) {
+                    if (!rowRegex.matches(lines[row]) || lines[row].replace(" ", "-") != lines.first())
+                        throw IllegalArgumentException("Row #$row is wrong")
+                }
+            }
+
+            //reading crosses positions
+            val crosses = mutableSetOf<Int>()
+            for (index in lines[0].indices) {
+                if (lines[0][index] == '+') {
+                    crosses.add(index)
+                }
+            }
+
+            //checking columns
+            fun rotate(index: Int) = lines.map { it[index] }.joinToString("")
+            val firstCol = rotate(0)
+            if (firstCol.any { it !in "+|" } || rotate(lines[0].length - 1).any { it !in "+|" })
+                throw IllegalArgumentException("Bad left/right border")
+            for (col in lines[0].indices) {
+                val line = rotate(col)
+                if (col in crosses) {
+                    if (!colRegex.matches(line))
+                        throw IllegalArgumentException("Column #$col is wrong")
+                }
+            }
+
+            val len = lines[0].length
+            val graph = Graph(crosses.size - 1, lines.size / 2)
+            var x = 0
+            var y = 0
+            //only rows with digits
+            for (row in 1 until lines.size step 2) {
+                val iterator = crosses.drop(1).iterator()
+                var cross = iterator.next()
+                val buffer = StringBuilder()
+
+                var col = 1
+                while (col < len) {
+                    if (col == cross) {
+                        //not including most left and right walls
+                        if (lines[row][col] == '|' && col < len - 1) {
+                            graph.walls[x + 1][y].left = true
+                            graph.walls[x][y].right = true
+                        }
+                        val value = buffer.replace(Regex("""\s"""), "")
+                        if (value.toIntOrNull() != null) graph.grid[x][y] = Node(value.toInt(), Position(x, y))
+                        else if (value == "S") {
+                            if (graph.start != null) throw IllegalArgumentException("Multiple start tiles")
+                            val node = Node(0, Position(x, y)).apply { isStart = true }
+                            graph.grid[x][y] = node
+                            graph.start = node
+                        }
+                        else if (value == "F") {
+                            if (graph.end != null) throw IllegalArgumentException("Multiple finish tiles")
+                            val node = Node(0, Position(x, y)).apply { isEnd = true }
+                            graph.grid[x][y] = node
+                            graph.end = node
+                        } else throw IllegalArgumentException("Invalid tile x:$x y:$y")
+                        buffer.clear()
+                        if (iterator.hasNext()) {
+                            cross = iterator.next()
+                        } else break
+                        x++
+                    } else {
+                        if (buffer.isEmpty()) {
+                            if (lines[row - 1][col] == '-') {
+                                graph.walls[x][y].up = true
+                            }
+                            if (lines[row + 1][col] == '-') {
+                                graph.walls[x][y].down = true
+                            }
+                        }
+                        buffer.append(lines[row][col])
+                    }
+                    col++
+                }
+                x = 0
+                y++
+            }
+            if (graph.start == null) throw IllegalArgumentException("No start present")
+            if (graph.end == null) throw IllegalArgumentException("No end present")
+            for (i in 0 until graph.height) {
+                graph.walls[0][i].left = true
+                graph.walls[graph.width - 1][i].right = true
+            }
+            for (i in 0 until graph.width) {
+                graph.walls[i][0].up = true
+                graph.walls[i][graph.height - 1].down = true
+            }
+            return graph
+        }
+    }
+
     fun solve(): Result {
         if (start == null || end == null) throw IllegalStateException("No end/start")
         link()
@@ -101,7 +208,7 @@ class Graph(val width: Int, val height: Int) {
         }
     }
 
-    fun toList(): List<Node> {
+    private fun toList(): List<Node> {
         val list = mutableListOf<Node>()
         for (row in grid) {
             for (node in row) {
@@ -112,6 +219,7 @@ class Graph(val width: Int, val height: Int) {
     }
 
     fun toLines(): List<String> {
+        if (start == null && end == null) throw IllegalStateException("No start or end present")
         val spaces = IntArray(width)
         for (y in 0 until height) {
             for (x in grid.indices) {
