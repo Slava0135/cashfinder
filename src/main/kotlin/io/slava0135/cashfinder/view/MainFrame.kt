2c30dbb297fb5754c952020e913c5d39c2e680e2
slava0135 <super.novalskiy_0135@inbox.ru>
2020-10-03 17:14:18 +0300

    saving loading

--- src/main/kotlin/io/slava0135/cashfinder/view/MainFrame.kt
+++ src/main/kotlin/io/slava0135/cashfinder/view/MainFrame.kt
@@ -1,28 +1,33 @@
 package io.slava0135.cashfinder.view
 
+import javafx.stage.FileChooser
 import tornadofx.*
 
 class Cashfinder: App(MainView::class)
 
 class MainView: View() {
     override val root = borderpane {
-        top<TopView>()
-        bottom<BottomView>()
+        top<Menu>()
+        bottom<Workspace>()
     }
 }
 
-class TopView: View() {
+class Menu: View() {
     override val root = menubar {
         menu("File") {
             item("New")
-            item("Save")
-            item("Load")
+            item("Save").action {
+                val file = chooseFile("Select Output File", arrayOf(FileChooser.ExtensionFilter("Cash File (*.csh)", "*.csh")), mode = FileChooserMode.Save)
+            }
+            item("Load").action {
+                val file = chooseFile("Select Input File", arrayOf(FileChooser.ExtensionFilter("Cash File (*.csh)", "*.csh")))
+            }
         }
         menu("Solve")
         menu("Help")
     }
 }
 
-class BottomView: View() {
+class Workspace: View() {
     override val root = label("WORKSPACE")
 }
